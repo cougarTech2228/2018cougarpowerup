65310805ca92c8aeaf07f46ca68f4cceaf99cec2
Brenden <brenden@hflcsd.org>
2018-01-29 19:44:32 -0500

    Made generic template for DistanceIF

---
+++ src/org/usfirst/frc/team2228/robot/DistanceIF.java
@@ -0,0 +1,58 @@
+package org.usfirst.frc.team2228.robot;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+public class DistanceIF extends SampleRobot{
+	AnalogInput sensor = new AnalogInput(1);
+	double voltage;
+	double distance;
+	
+	//temporary return value for the double methods
+	double a;
+	//PIDController to be used for getPerpindicularDistanceCorrection()
+	PIDController ultraSensor;
+	//creates the ultra object and assigns ultra to be an ultrasonic sensor which uses DigitalOutput 1 for the echo pulse and DigitalInput 1 for the trigger pulse
+	Ultrasonic ultra = new Ultrasonic(1, 1);
+	// If true, sensor is on the bot and enabled. If false, sensor is not on the bot and/or the sensor is not enabled
+	boolean isSensorEnabled = false;
+
+	public DistanceIF(AnalogInput sensor) {
+		
+	}
+	
+	public void setCalibrationNear(){
+		
+	}
+	
+	public void setCalibrationFar(){
+		
+	}
+	
+	public double getLeftDistance(){
+		return a;
+	}
+	
+	public double getLeftFilteredDistance(){
+		return a;
+	}
+	
+	public double getRightDistance(){
+		return a;
+	}
+	
+	public double getRightFilteredDistance(){
+		return a;
+	}
+	
+	public void getPerpendicularDistanceCorrection(){
+		
+	}
+	
+	public double initDistance(){
+		voltage = sensor.getVoltage();
+		return voltage;
+	}
+}
