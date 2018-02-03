e10d42b1c2e788876102e7f3fc026284655b3969
Brenden <brenden@hflcsd.org>
2018-02-03 13:56:50 -0500

    adding in Kevin's dimensions

---
+++ src/org/usfirst/frc/team2228/robot/Dimensions.java
@@ -0,0 +1,72 @@
+package org.usfirst.frc.team2228.robot;
+
+public class Dimensions {
+	/*
+	 * NOTE:
+	 * 	These are in inches
+	 *	Length is always the longer dimension, width is the shorter
+	 * 	Information from game manual, Section 3 Arcade 3.1-
+	 *  Link to source: https://firstfrc.blob.core.windows.net/frc2018/Manual/Sections/03-Arcade.pdf
+	 */
+
+	
+	//Field dimensions
+	public static final double AUTOLINE_WIDTH = 2;
+	public static final double AUTOLINE_TO_ALIENCE = 120;
+	public static final double EXCHANGEZONE_WIDTH = 36;
+	public static final double EXCHANGEZONE_LENGTH = 48;
+	public static final double ALL_TAPE_WIDTH = 2;
+	public static final double NULL_TERRITORY_LENGTH = 95.25;
+	public static final double NULL_TERRITORY_WIDTH = 72;
+	public static final double POWER_CUBE_ZONE_LENGTH = 45;
+	public static final double POWER_CUBE_ZONE_WIDTH = 42;
+	public static final double TOTAL_FIELD_LENGTH = 648;
+	public static final double TOTAL_FIELD_WIDTH = 324;
+	
+	//Scale Dimensions
+	public static final double SCALE_HIGHEST_POSITION = 72;
+	public static final double SCALE_LOWEST_POSITION = 48;
+	public static final double SCALE_STARTING_POSITION = 60;
+	public static final double SCALE_OWNERSHIP_BEGINS = 56;
+	public static final double SCALE_PLATFORM_BACK_PLATE = 15;
+	public static final double SCALE_PLATFORM_WALL = 3.5;
+	public static final double SCALE_PLATFORM_WIDTH = 36;
+	public static final double SCALE_PLATFORM_LENGTH = 48;
+	public static final double END_OF_SCALE_TO_OTHER_END = 180;
+	
+	//Tower and Rungs Dimensions
+	public static final double TOWER_WIDTH = 17;
+	public static final double TOWER_EXTENSION_FROM_OUTTRIGGERS = 2;
+	public static final double TOP_OF_RUNG_TO_CARPET = 84; // NOT TO RAMP, TO CARPET!
+	public static final double TOWER_TO_RUNG = 8.25;
+	public static final double RUNG_LENGTH = 13;
+	
+	//Platform Dimensions
+	public static final double PLATFORM_TOP_WIDTH = 41.25;
+	public static final double PLATFORM_TOP_LENGTH = 104;
+	public static final double PLATEFORM_HIGHT = 3.5;
+	public static final double RAMP_TO_PLATFORM_LENGTH = 12.75;
+	public static final double RAMP_ANGLE = 15.35; // NOT IN INCHES, IT IS AN ANGLE!
+	
+	//Switch Dimensions
+	public static final double SWITCHWALL_TO_ALLIANCESTATION= 168;
+	public static final double SWITCHPLATE_LENGTH = 48;
+	public static final double SWITCHPLATE_WIDTH = 36;
+	public static final double SWITCHPLATE_EDGE_TO_SWITCHPLATE_EDGE = 144;
+	public static final double SWITCHPLATE_STARTINGHEIGHT = 9;
+	public static final double SWITCHPLATE_LOWEST_POSITION = 3;
+	public static final double SWITCHPLATE_TO_SIDESWITCHWALL = 1.5;
+	public static final double SWITCHPLATE_TO_FRONTSWITCHWALL = 2.25;
+	public static final double SWITCHWALL_HIGHT = 18.75;
+	public static final double SWITCHWALL_TO_SWITCHWALL_LENGTH = 153.5;
+	public static final double SWITCHWALL_TO_SWITCHWALL_WIDTH = 56;
+	
+	//Exchange zone dimensions
+	public static final double GROUNG_TO_RETURN_BOTTEM = 20;
+	public static final double BOTTEM_OF_RETURN_TO_TOP = 14;
+	public static final double HIGHT_OF_RAMP = 1.75;
+	public static final double BOTTEM_OF_INPUT_TO_TOP_OF_INPUT = 16.25;
+	public static final double WIDTH_OF_INPUT = 21;
+	
+	
+}
