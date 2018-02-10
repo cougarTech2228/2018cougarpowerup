18499a762c2f8a141c997d69e537cae2991f52dc
Cougartech Software <Cougartechsoftware@gmail.com>
2018-02-10 10:36:05 -0500

    added a CAN controlled LED display

--- src/org/usfirst/frc/team2228/robot/CANLED.java
+++ src/org/usfirst/frc/team2228/robot/CANLED.java
@@ -18,17 +18,18 @@ public class CANLED {
 		ds2 = ds.getInstance();
 	}
 
-	// public void allianceColorLED() {
-	// if (ds2.getAlliance() == DriverStation.Alliance.Red) {
-	// System.out.println("Red Alliance Detected");
-	// frameLights.showRGB(255, 0, 0);
-	// } else if (ds2.getAlliance() == DriverStation.Alliance.Blue) {
-	// System.out.println("Blue Alliance Detected");
-	// frameLights.showRGB(0, 0, 255);
-	// } else if (ds2.getAlliance() == DriverStation.Alliance.Invalid) {
-	// System.out.println("No Alliance Detected");
-	// frameLights.showRGB(255, 200, 0); // yellow
-	// }
+	public void allianceColorLED() {
+		if (ds2.getAlliance() == DriverStation.Alliance.Red) {
+			System.out.println("Red Alliance Detected");
+			frameLights.showRGB(255, 0, 0);
+		} else if (ds2.getAlliance() == DriverStation.Alliance.Blue) {
+			System.out.println("Blue Alliance Detected");
+			frameLights.showRGB(0, 0, 255);
+		} else if (ds2.getAlliance() == DriverStation.Alliance.Invalid) {
+			System.out.println("No Alliance Detected");
+			frameLights.showRGB(255, 200, 0); // yellow
+		}
+	}
 
 	public void autonomousColorInit() {
 		USAColor();
@@ -45,12 +46,12 @@ public class CANLED {
 
 	public void rainbowShift() {
 
-//		for(g = 0; g < 255; g++){
-//			frameLights.showRGB(r, g, b);
-//		}
-//		for(r = 255; r > 0; r--){
-//			frameLights.showRGB(r, g, b);
-//		}
+		// for(g = 0; g < 255; g++){
+		// frameLights.showRGB(r, g, b);
+		// }
+		// for(r = 255; r > 0; r--){
+		// frameLights.showRGB(r, g, b);
+		// }
 		if (r == 255 && g < 255 && b == 0) {
 			frameLights.showRGB(r, g, b);
 			g++;
@@ -60,7 +61,7 @@ public class CANLED {
 		}
 		if (g == 255 && r > 0 && b == 0) {
 			frameLights.showRGB(r, g, b);
-			r--;		
+			r--;
 			System.out.println(r);
 			System.out.println(g);
 			System.out.println(b);
@@ -93,6 +94,6 @@ public class CANLED {
 			System.out.println(g);
 			System.out.println(b);
 		}
-		
+
 	}
 }
