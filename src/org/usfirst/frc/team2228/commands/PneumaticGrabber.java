5403901c16ad66b99fea2d8c2cfbd97124066fa2
nbradley19 <nathanjbradley19@gmail.com>
2018-02-20 21:38:15 -0500

    pneumatic command

---
+++ src/org/usfirst/frc/team2228/commands/PneumaticGrabber.java
@@ -0,0 +1,59 @@
+package org.usfirst.frc.team2228.commands;
+
+import org.usfirst.frc.team2228.robot.DebugLogger;
+import org.usfirst.frc.team2228.robot.PneumaticController;
+import org.usfirst.frc.team2228.robot.RobotMap;
+import org.usfirst.frc.team2228.robot.SRXDriveBase;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class PneumaticGrabber extends Command {
+		private PneumaticController pneu;
+		private boolean grabbing;
+	
+	
+	public PneumaticGrabber(PneumaticController _pneu, boolean _grab, double timeWait) {
+		//It's actually ahhhhhhhhhhhhhhhhhhh
+		super(timeWait);
+		pneu = _pneu;
+		grabbing = _grab;
+	}
+	
+	protected void initialize() {
+		if(grabbing == true){
+			System.out.println("Grabbing Started At " + Timer.getFPGATimestamp());
+			
+		}
+		else{
+			System.out.println("Not Grabbing At " + Timer.getFPGATimestamp());
+		}
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	public void execute() {
+		pneu.squeeze(grabbing);
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return isTimedOut();
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		if(grabbing == true){
+			System.out.println("Grabbing Stopped At " + Timer.getFPGATimestamp());
+		}
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+
+}
